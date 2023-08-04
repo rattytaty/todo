7aa5b91ed50c30321ebcc5cd0e6660a50387a0c4
Daniel <dmolodovskyi@gmail.com>
2023-08-04 03:18:40 +0200

    Global rework

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans':['Nunito'],
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        myLightTheme: {
+          "background": "rgba(207,213,183,0.89)",
+          "neutral": "#262a2d",
+          "primary": "#641ae6",
+          "secondary": "#d926a9",
+          "accent": "#1fb2a6",
+          "base-100": "#ffffff",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 }
